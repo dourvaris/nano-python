63bbed381e6c16f657a7c2b3d92225d901ce790c
Laurențiu Andronache <laurentiu.andronache@trailung.ro>
2019-02-09 23:11:43 +0000

    fix bug with dict mentioned in utilities.rst not being available

--- nano/__init__.py
+++ nano/__init__.py
@@ -1,5 +1,5 @@
 from .rpc import RPCClient, RPCException
-from .conversion import convert
+from .conversion import convert, UNITS_TO_RAW
 from .accounts import (
     KNOWN_ACCOUNT_IDS, KNOWN_ACCOUNT_NAMES, generate_account
 )
